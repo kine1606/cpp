483d7ff2a42acd16e0921084d6d019529c214460
kine1606 <ntkien160605@gmail.com>
2023-08-16 21:27:28 +0700

    add tinhtong4.cpp

---
+++ cpp/tinhtong4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+  int main()
+  {
+    double s(0);
+    double i(2);
+    int n;
+    cout<< "nhap n chan = ";
+    do
+    {
+        cin>> n;
+    } while (n%2 !=0);
+    while (i<=n)
+    {
+        s=s+ 1/i;
+        i+=2;
+    }
+    
+    cout<< "s="<< s;
+  }
