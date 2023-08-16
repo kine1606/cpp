dede1fac9d71ce224cd0be3763e93c67f0166de3
kine1606 <ntkien160605@gmail.com>
2023-08-16 21:04:41 +0700

    add tinhtong1.cpp

--- cpp/tinhtong.cpp
+++ cpp/tinhtong.cpp
@@ -1,8 +1,17 @@
-#include <iostream>   
-#include <array>   
+#include <iostream>
 using namespace std;
   int main()
   {
-    
-  }
+    int s(0);
+    int i(1);
+    int n;
+    cin>>n;
+     while (i<=n)
+      {
+        s=s+i;
+        i++; 
+      }
+    cout<< "s="<< s;
 
+  }
+  
