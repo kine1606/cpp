2a1c843b734c0b7e5346a492b786eb08497ca351
kine1606 <ntkien160605@gmail.com>
2023-08-17 01:48:12 +0700

    add tinhmu11.cpp

---
+++ cpp/tinhmu11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+  int main()
+  { // nhap chuong trinh tinh theo bai 11 cua trang studocu.com by Kine1606
+     long T(1);
+     int x;
+     int n;
+     cout<< "nhap so x=";
+     cin>> x;
+     cout<< "nhap so n=";
+     cin>> n;
+     T= pow(x,n) ;
+    cout<< "T="<< T;
+  }
