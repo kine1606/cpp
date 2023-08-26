03898d034c74afe13caf2da2ed8d08d24325ab1f
kine1606 <ntkien160605@gmail.com>
2023-08-26 18:52:05 +0700

    add nmlt5.cpp

---
+++ cpp/training/nmlt5.cpp
@@ -0,0 +1,18 @@
+//S theo công thức = 1 + 1/2 + 1/3 + 1/4 + ... + 1/N . by Kine1606
+
+//Xem nội dung đầy đủ tại: https://123docz.net/document/3504891-de-thi-nhap-mon-lap-trinh-c-co-giai-chi-tiet.htm
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+   double S(0);
+   int n;
+   cout << " nhap so n=";
+   cin>> n;
+   for (int i = 1; i <=n; i++)
+   {
+    S= S + 1.0/i;
+   }
+      cout << " tong s=" << S;
+}
