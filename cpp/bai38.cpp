917aab7f6e7ca234e52e694aa0f8dbc9e7aa18b6
kine1606 <ntkien160605@gmail.com>
2023-09-10 14:38:44 +0700

    add bai38.cpp

---
+++ cpp/bai38.cpp
@@ -0,0 +1,21 @@
+//Hãy đếm số lượng chữ số của số nguyên dương n.
+//by Kine1606
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long n(0);
+    cout << " enter n =";
+    cin >> n;
+    int count (1);
+    for (int i = 10; i <n; i*=10)
+    {
+        if (n/10 !=0)
+        {
+            count++;
+        }
+        else break;
+    }
+    cout << n << " have " << count << " number(s)";
+    return 0;
+}
