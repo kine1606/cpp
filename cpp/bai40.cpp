a97ce8fc3c034f29a5be80dd5950b311804edf76
kine1606 <ntkien160605@gmail.com>
2023-09-12 20:55:32 +0700

    add bai40.cpp

---
+++ cpp/bai40.cpp
@@ -0,0 +1,18 @@
+//Hãy tính tích các chữ số của số nguyên dương n.
+//by Kine1606
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long n(0);
+    cout << "enter n =";
+    cin >> n;
+    int multiple(1);
+    while (n!=0)
+    {
+        multiple*=n%10;
+        n/=10;
+    }
+    cout << "sum all elements of n= " << multiple;
+    return 0;
+}
