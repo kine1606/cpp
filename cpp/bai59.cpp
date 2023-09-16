fb209a04d3b4aab39e7dd7f19f606901711a6c77
kine1606 <ntkien160605@gmail.com>
2023-09-16 19:30:34 +0700

    add bai59.cpp

--- cpp/bai59.cpp
+++ cpp/bai59.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main()
 {
-    int a,b,c;
+    double a,b,c;
     cout << "enter a=";
     cin >> a;
     cout << "enter b=";
@@ -29,7 +29,7 @@ int main()
         }
         else 
         {
-            cout << " nghiem x=" << (-c/b*1.0);
+            cout << " nghiem x=" << (-c/b);
             return 0;
         }
     }
