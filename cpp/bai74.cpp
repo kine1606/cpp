fdb56af4a36e07ce16c92999ae5b9186b7c66591
kine1606 <ntkien160605@gmail.com>
2023-09-18 11:53:36 +0700

    add bai74.cpp

---
+++ cpp/bai74.cpp
@@ -0,0 +1,12 @@
+//  Hãy sử dụng vòng lặp for để xuất tất cả các ký tự từ A đến Z. 
+// by Kine1606
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (char i = 'A'; i <= 'Z'; i++)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
