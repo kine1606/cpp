d4f8898811bc5112e5c85fb037e37b2322e450aa
kine1606 <ntkien160605@gmail.com>
2023-09-18 11:44:25 +0700

    add bai73.cpp

---
+++ cpp/bai73.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int S(0);
+    for (int i = 1; i <5000; i++)
+    {  
+        S+=i;
+        if (S>10000)
+        {
+            cout << S << '\n';
+            cout << "number n is " << i;
+            break;
+        }
+    }
+    return 0;
+}
