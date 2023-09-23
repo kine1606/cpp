ef3869b10d68cfd492d55fd67fa475599db07896
kine1606 <ntkien160605@gmail.com>
2023-09-23 23:49:45 +0700

    add bai88.cpp

---
+++ cpp/bai88.cpp
@@ -0,0 +1,15 @@
+//Tính S = xy
+//by Kine1606
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double x,y;
+    cout << "enter x=";
+    cin >> x;
+    cout << "enter y=";
+    cin >>y;
+    cout << "S=" << pow(x,y);
+    return 0;
+}
