16f452c125e885b393915095e9e8587e958ec118
kine1606 <ntkien160605@gmail.com>
2024-01-07 15:12:39 +0700

    add chanle.cpp

---
+++ cpp/training/wecode/chanle.cpp
@@ -0,0 +1,11 @@
+// in ra 0 neu n chan or 1 neu n le
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    (n%2==0)? cout << "0" : cout << "1"; 
+    return 0;
+    
+}
