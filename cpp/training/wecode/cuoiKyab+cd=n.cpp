225545ba48450879bc5f1396aaa54c9209c10976
kine1606 <ntkien160605@gmail.com>
2024-01-07 15:15:29 +0700

    add cuoikyab+cd=n.cpp

---
+++ cpp/training/wecode/cuoiKyab+cd=n.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int finding(int n)
+{
+    int count(0);
+    for (size_t i = 1; i <=n; i++)
+        if(n%i==0) count++;
+    return count;
+}
+int foundWay(int n)
+{
+    int way(0);
+    for (size_t i = 1; i <n; i++)
+        way+=finding(i)*finding(n-i);
+    return way;
+}
+
+int main()
+{ 
+    int n;
+    std::cin>> n ;
+    std::cout << foundWay(n);
+}
